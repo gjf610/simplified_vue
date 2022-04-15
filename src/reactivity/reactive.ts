c1489b23cb173489b867b0f7035bdaa8dfcb3f52
jianfeng <gjf610@yeah.net>
2022-04-16 00:15:02 +0800

    refactor: reactive createGetter & createSetter

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,28 +1,35 @@
 import { track, trigger } from "./effect"
 
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key)
+    // 依赖收集
+    if (!isReadonly) {
+      track(target, key)
+    }
+    return res
+  }
+}
+
+function createSetter(isReadonly = false) {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value)
+    // 触发依赖
+    trigger(target, key)
+    return res;
+  }
+}
+
 export function reactive(raw) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-      // 依赖收集
-      track(target, key)
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-      // 触发依赖
-      trigger(target, key)
-      return res;
-    }
+    get: createGetter(),
+    set: createSetter()
   })
 }
 
 export function readonly(raw) {
   return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-      return res
-    },
+    get: createGetter(true),
     set(target, key, value) {
       return true
     }
