d588560f01d8ed71fc0fda5e9743a4d07a3bdb33
jianfeng <gjf610@yeah.net>
2022-04-16 00:21:51 +0800

    refactor: reactive mutableHandles & readonlyHandles

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,37 +1,9 @@
-import { track, trigger } from "./effect"
-
-function createGetter(isReadonly = false) {
-  return function get(target, key) {
-    const res = Reflect.get(target, key)
-    // 依赖收集
-    if (!isReadonly) {
-      track(target, key)
-    }
-    return res
-  }
-}
-
-function createSetter(isReadonly = false) {
-  return function set(target, key, value) {
-    const res = Reflect.set(target, key, value)
-    // 触发依赖
-    trigger(target, key)
-    return res;
-  }
-}
+import { mutableHandles, readonlyHandles } from "./baseHandlers"
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get: createGetter(),
-    set: createSetter()
-  })
+  return new Proxy(raw, mutableHandles)
 }
 
 export function readonly(raw) {
-  return new Proxy(raw, {
-    get: createGetter(true),
-    set(target, key, value) {
-      return true
-    }
-  })
+  return new Proxy(raw, readonlyHandles)
 }
