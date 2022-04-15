7d3817a46a54583e89b482a77cb6bb72951ea505
jianfeng <gjf610@yeah.net>
2022-04-16 01:00:53 +0800

    feat: isReactive & isReadonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,5 +1,10 @@
 import { mutableHandles, readonlyHandles } from "./baseHandlers"
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly"
+}
+
 export function reactive(raw) {
   return createActiveObject(raw, mutableHandles)
 }
@@ -7,7 +12,12 @@ export function reactive(raw) {
 export function readonly(raw) {
   return createActiveObject(raw, readonlyHandles)
 }
-
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE]
+}
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY]
+}
 function createActiveObject(raw: any, baseHandlers) {
   return new Proxy(raw, baseHandlers)
 }
