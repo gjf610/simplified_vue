99d6bc6c7558483a2f14f5e1a806a33f793e8e08
jianfeng <gjf610@yeah.net>
2022-04-15 21:10:20 +0800

    feat: effect scheduler

--- src/reactivity/test/effect.spec.ts
+++ src/reactivity/test/effect.spec.ts
@@ -29,4 +29,32 @@ describe('effect', () => {
     expect(foo).toBe(12)
     expect(r).toBe("foo")
   })
+
+  it('scheduler', () => {
+    // 1. 通过 effect 的第二个参数给定的一个 scheduler 的fn
+    // 2. effect第一次执行的时候还会执行fn
+    // 3. 当响应式对象set update 不会执行fn而是执行scheduler
+    // 4. 如果说当执行runner的时候，会再次执行fn
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(() => {
+      dummy = obj.foo
+    }, { scheduler })
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
+  })
 })
