d9bba7f7a3356e974d04e50251e8db62e27c7402
jianfeng <gjf610@yeah.net>
2022-04-16 22:04:07 +0800

    feat: object embedded change for reactive & readonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
 import { track, trigger } from "./effect"
-import { ReactiveFlags } from "./reactive"
+import { reactive, ReactiveFlags, readonly } from "./reactive"
+import { isObject } from "./shared"
 
 const get = createGetter()
 const set = createSetter()
@@ -14,6 +15,13 @@ function createGetter(isReadonly = false) {
       return isReadonly
     }
     const res = Reflect.get(target, key)
+
+    //看看res是不是object
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+
+
     // 依赖收集
     if (!isReadonly) {
       track(target, key)
