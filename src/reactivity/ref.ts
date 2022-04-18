8127fddb2fb45cd88a70509cea042a9cc2a50cf9
jianfeng <gjf610@yeah.net>
2022-04-18 22:01:45 +0800

    feat: isRef & unRef

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   public dep;
   private _rewValue: any;
+  public __v_isRef = true
   constructor(value) {
     this._rewValue = value
     this._value = convert(value)
@@ -39,4 +40,11 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef
+}
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref
 }
